14cae7550e97deae9b7fe20a55d402468df89cda
Weiming Zhai <wmzhai@gmail.com>
2015-06-22 17:33:02 +0800

    add count

--- todos.js
+++ todos.js
@@ -61,6 +61,15 @@ if(Meteor.isClient){
             }
         }
     });
+
+    Template.todosCount.helpers({
+        'totalTodos' : function(){
+            return Todos.find().count();
+        },
+        'completedTodos' : function(){
+            return Todos.find({completed : true}).count();
+        }
+    });
 }
 
 
